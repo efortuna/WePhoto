f3e78bdf205ff0236a3ac98480b85b432b8ac243
Emily <efortuna@google.com>
2014-08-18 15:43:04 -0700

    Added three buttons that do NOTHING.

--- WePhoto/app/src/main/java/com/google/hackathon/wephoto/SelectMode.java
+++ WePhoto/app/src/main/java/com/google/hackathon/wephoto/SelectMode.java
@@ -112,7 +112,7 @@ public class SelectMode extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.gallery).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
