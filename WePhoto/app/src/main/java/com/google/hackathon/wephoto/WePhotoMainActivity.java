4c61d0e2712dd866f4c2578f1e728c0a8be389a8
Jacob Macdonald <jakemac@jakemac-macbookpro.roam.corp.google.com>
2014-08-19 11:13:48 -0700

    main activity now connects to google drive

--- WePhoto/app/src/main/java/com/google/hackathon/wephoto/WePhotoMainActivity.java
+++ WePhoto/app/src/main/java/com/google/hackathon/wephoto/WePhotoMainActivity.java
@@ -2,16 +2,32 @@ package com.google.hackathon.wephoto;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 
-public class WePhotoMainActivity extends FragmentActivity implements ActionBar.TabListener {
+public class WePhotoMainActivity extends FragmentActivity implements
+        ActionBar.TabListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private ViewPager viewPager;
     private TabsPagerAdapter mAdapter;
     private ActionBar actionBar;
+
+    /**
+     * Google API client.
+     */
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +71,64 @@ public class WePhotoMainActivity extends FragmentActivity implements ActionBar.T
             public void onPageScrollStateChanged(int arg0) {
             }
         });
+
+        /**
+        * Set up google drive connection.
+        */
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addApi(Drive.API)
+            .addScope(Drive.SCOPE_FILE)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is connected.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i("", "GoogleApiClient connected");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("", connectionResult.toString());
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(
+                        this, ConnectionResult.RESOLUTION_REQUIRED);
+            } catch (IntentSender.SendIntentException e) {
+                // Unable to resolve, message user appropriately
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
+    }
+
+    /**
+     * Called when {@code mGoogleApiClient} is disconnected.
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i("", "GoogleApiClient connection suspended");
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case ConnectionResult.RESOLUTION_REQUIRED:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
     }
 
     @Override
