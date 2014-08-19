27616a59ebd389efd961d9b2be63137cef883cd9
Jacob Macdonald <jakemac@jakemac-macbookpro.roam.corp.google.com>
2014-08-19 15:12:44 -0700

    simple events view with adaptor

--- WePhoto/app/src/main/java/com/google/hackathon/wephoto/PreviousEventsFragment.java
+++ WePhoto/app/src/main/java/com/google/hackathon/wephoto/PreviousEventsFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.google.hackathon.wephoto.R;
 
@@ -13,8 +14,12 @@ public class PreviousEventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_previous_events, container, false);
+
+        ListView myListView = (ListView) rootView.findViewById(R.id.previous_events);
+        EventsAdapter resultsAdapter = new EventsAdapter(this.getActivity());
+        myListView.setAdapter(resultsAdapter);
+        ((WePhotoMainActivity) this.getActivity()).listRootFolderContents(resultsAdapter);
 
         return rootView;
     }
