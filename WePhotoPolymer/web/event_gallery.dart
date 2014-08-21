07b37a53ea3c67006275e8740efe2707d2cbfa75
Jacob Macdonald <jakemac@jakemac-macbookpro.roam.corp.google.com>
2014-08-21 10:20:27 -0700

    broke out each page into its own element

---
+++ WePhotoPolymer/web/event_gallery.dart
@@ -0,0 +1,39 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer event-gallery element.
+ */
+@CustomTag('event-gallery')
+class EventGallery extends PolymerElement {
+
+  /// Constructor used to create instance of EventGallery.
+  EventGallery.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of event-gallery is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of event-gallery is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// event-gallery is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when event-gallery has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
---
+++ WePhotoPolymer/web/take_photo.dart
@@ -0,0 +1,39 @@
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer take-photo element.
+ */
+@CustomTag('take-photo')
+class TakePhoto extends PolymerElement {
+
+  /// Constructor used to create instance of TakePhoto.
+  TakePhoto.created() : super.created() {
+  }
+
+  /*
+   * Optional lifecycle methods - uncomment if needed.
+   *
+
+  /// Called when an instance of take-photo is inserted into the DOM.
+  attached() {
+    super.attached();
+  }
+
+  /// Called when an instance of take-photo is removed from the DOM.
+  detached() {
+    super.detached();
+  }
+
+  /// Called when an attribute (such as  a class) of an instance of
+  /// take-photo is added, changed, or removed.
+  attributeChanged(String name, String oldValue, String newValue) {
+  }
+
+  /// Called when take-photo has been fully prepared (Shadow DOM created,
+  /// property observers set up, event listeners attached).
+  ready() {
+  }
+   
+  */
+  
+}
