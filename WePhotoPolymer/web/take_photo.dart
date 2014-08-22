51fd4fb0db6e9f954439206309de639ee79a8e12
Jacob Macdonald <jakemac@jakemac-macbookpro.roam.corp.google.com>
2014-08-22 11:00:21 -0700

    make snap photo button look nicer

--- WePhotoPolymer/web/take_photo.dart
+++ WePhotoPolymer/web/take_photo.dart
@@ -1,4 +1,5 @@
 import 'package:polymer/polymer.dart';
+import 'package:paper_elements/paper_button.dart';
 import 'google_drive.dart';
 
 import 'dart:html';
@@ -52,18 +53,19 @@ class TakePhoto extends PolymerElement {
 
      VideoElement video = shadowRoot.querySelector('video');
      CanvasElement canvas = shadowRoot.querySelector('#takenPhoto');
-     ButtonElement button = shadowRoot.querySelector('#accept');
+     PaperButton button = shadowRoot.querySelector('#accept');
      var ctx = canvas.getContext('2d');
      var localMediaStream = null;
      var takePhoto = true;
          
-     button.onClick.listen((MouseEvent event) {
+     button.on['click'].listen((MouseEvent event) {
        if (localMediaStream != null && takePhoto) { 
          canvas.width = min(640, video.clientWidth);
          canvas.height = ((video.clientHeight / video.clientWidth) * canvas.width).floor();
          ctx.drawImageScaled(video, 0, 0, canvas.width, canvas.height);
          video.pause();
-         button.innerHtml = 'Uploading...';
+         button.label = 'Uploading...';
+         button.disabled = true;
          takePhoto = false;
          // TODO: send the picture in "ctx" off to Google Docs!
          new GoogleDrive().uploadDrive.then((drive) {
@@ -74,7 +76,8 @@ class TakePhoto extends PolymerElement {
                'multipart/related;', queryParams: params).then((response) {
              // Restore the button to its original state and let you take more photos.
              video.play();
-             button.innerHtml = 'Snap Photo';
+             button.label = 'Snap Photo';
+             button.disabled = false;
              takePhoto = true;
              print(response);
            });
